80001d431bc2bd6d3e7c0fe146565343f7c6eefd
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-11-04 13:38:54 -0500

    using NavLink

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,33 +1,26 @@
 import React from "react";
 import "../header/header.css"
+import { NavLink } from "react-router-dom";
 
 function Header(props) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light ">
-  <a className="navbar-brand" href="/">Garrett Stein-Seroussi</a>
+    <nav className="navbar navbar-expand-lg navbar-light "><span>
+      <NavLink to="/">Garrett Stein-Seroussi</NavLink></span>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
   <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav">
-      <li className="nav-item active">
-        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+      <li className="nav-item"><span>
+      <NavLink to="/Portfolio">Portfolio</NavLink></span>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="/Portfolio">Portfolio</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/Contact">Contact</a>
+        <span>
+      <NavLink to="/Contact">Contact</NavLink>
+      </span>
       </li>
       <li>
-      <a
-        className="nav-link"
-        href={props.href}
-        download="Stein-Seroussi Resume"
-        target="_blank"
-      >
-        Download Resume
-      </a>
+      <NavLink to={props.href} download="Stein-Seroussi Resume" target="_blank">Download Resume</NavLink>
       </li>
     </ul>
   </div>
